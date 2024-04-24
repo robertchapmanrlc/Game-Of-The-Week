de94d94ef61adb8b7605c80c1e540e0cc2d1f1cd
Robert Chapman <chapmanrobert12@gmail.com>
2024-04-24 14:46:08 -0500

    Modified tailwind config for colors and fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,18 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        bebasneue: ["var(--font-bebas-neue)"],
+      },
+      colors: {
+        text: "#ebe9fc",
+        background: "#010104",
+        primary: "#3a31d8",
+        secondary: "#020024",
+        accent: "#0600c2",
       },
     },
   },
