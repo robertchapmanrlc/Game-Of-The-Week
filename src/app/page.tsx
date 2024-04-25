db9647d04031342ba2977b4b89c5cd371e88fcc7
Robert Chapman <chapmanrobert12@gmail.com>
2024-04-25 16:34:51 -0500

    Added css media query for smaller screens. Added padding for larger screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="w-full fixed inset-0 flex flex-col items-center justify-center gap-y-5 px-8">
+    <main className="w-full sm:py-20 md:py-10 lg:py-5 flex flex-col items-center justify-center gap-y-5 px-8">
       <h1 className="text-text font-bebasneue text-3xl md:text-5xl lg:text-7xl">
         Game of the Week
       </h1>
