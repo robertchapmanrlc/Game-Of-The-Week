dad068d28837b8f23210ab1c038fff6081e5a9ba
Robert Chapman <chapmanrobert12@gmail.com>
2024-04-26 21:24:25 -0500

    Clicking on the vote buttons will increment the votes for a game and disable the button

--- src/app/components/game.tsx
+++ src/app/components/game.tsx
@@ -1,4 +1,7 @@
+'use client'
+
 import Image from "next/image";
+import { useState } from "react";
 
 type Game = {
   name: string;
@@ -7,6 +10,15 @@ type Game = {
 };
 
 export default function Game({ game, id }: { game: Game; id: number }) {
+
+  const [votes, setVotes] = useState(game.votes);
+  const [voted, setVoted] = useState(false);
+
+  const handleClick = () => {
+    setVotes((vote) => vote + 1);
+    setVoted(true);
+  };
+
   return (
     <li className="flex flex-col items-center gap-y-5 md:gap-y-8">
       <Image
@@ -18,10 +30,12 @@ export default function Game({ game, id }: { game: Game; id: number }) {
         priority
       />
       <button
+        onClick={() => handleClick()}
         data-testid={`Game ${id} Vote`}
-        className="w-full py-1 md:py-2 text-lg md:text-xl lg:text-3xl text-text font-bebasneue bg-primary rounded-md hover:scale-105 transition-transform"
+        disabled={voted}
+        className="w-full py-1 md:py-2 text-lg md:text-xl lg:text-3xl text-text font-bebasneue bg-primary rounded-md hover:scale-105 transition-transform disabled:bg-accent disabled:hover:scale-100"
       >
-        Vote
+        {voted ? votes : <>Vote</>}
       </button>
       <h3 className="text-text text-center text-lg md:text-xl lg:text-3xl font-bebasneue">
         {game.name}
