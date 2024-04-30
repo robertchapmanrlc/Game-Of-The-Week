cee241ce20b2f205cc38215b6df9d14b350d951f
Robert Chapman <chapmanrobert12@gmail.com>
2024-04-30 18:43:29 -0500

    Created auth route for signing in and signing up. Users now sign in from the landing page to get to the voting page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from "next";
 import { Bebas_Neue } from "next/font/google";
-import { ClerkProvider } from "@clerk/nextjs";
+import { ClerkProvider, UserButton } from "@clerk/nextjs";
 
 import "./globals.css";
 
 const bebas_neue = Bebas_Neue({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-bebas-neue',
-  weight: '400'
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-bebas-neue",
+  weight: "400",
 });
 
 export const metadata: Metadata = {
@@ -24,7 +24,12 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${bebas_neue.variable} bg-background`}>{children}</body>
+        <body className={`${bebas_neue.variable} bg-background`}>
+          <div className="absolute right-4 top-4">
+            <UserButton />
+          </div>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,36 @@
 import { auth } from "@clerk/nextjs/server";
-
-import { getGames } from "@/server/queries";
-import Games from "./components/games";
+import { SignedOut, SignInButton } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
 
   const { userId } = auth();
 
-  if (!userId) {
-    redirect('/sign-in');
+  if (userId) {
+    redirect("/voting");
   }
-
-  const games = await getGames();
 
   return (
     <main className="w-full sm:py-20 md:py-10 lg:py-5 flex flex-col items-center justify-center gap-y-5 px-8">
       <h1 className="text-text font-bebasneue text-3xl md:text-5xl lg:text-7xl">
         Game of the Week
       </h1>
-      <Games games={games} />
+      <div className="flex flex-col justify-center items-center gap-y-10">
+        <h2 className="text-gray-400 font-bebasneue text-center text-sm md:text-xl lg:text-2xl">
+          Not quite game of the year, but there is still value in being game of
+          the week right?
+        </h2>
+        <SignedOut>
+          <SignInButton
+            forceRedirectUrl="/voting"
+            signUpFallbackRedirectUrl="/voting"
+          >
+            <button className="w-24 bg-primary p-2 rounded-md text-text">
+              Sign In
+            </button>
+          </SignInButton>
+        </SignedOut>
+      </div>
     </main>
   );
 }
