69245e95640619fa5d03e9beb9447b7486efafef
Robert Chapman <chapmanrobert12@gmail.com>
2024-05-01 09:43:39 -0500

    Modified the testing file for the landing page

--- tests/testing.spec.ts
+++ tests/testing.spec.ts
@@ -5,9 +5,9 @@ test.beforeEach(async ({ page }) => {
 });
 
 test("Layout test", async ({ page }) => {
-  await expect(page.getByRole('heading', { name: 'Game of the Week' })).toBeVisible();
-  await expect(page.getByRole('img', { name: 'Game 1' })).toBeVisible();
-  await expect(page.getByRole('img', { name: 'Game 2' })).toBeVisible();
-  await expect(page.getByTestId('Game 1 Vote')).toBeVisible();
-  await expect(page.getByTestId("Game 2 Vote")).toBeVisible();
+  await expect(
+    page.getByRole("heading", { name: "Game of the Week", exact: true })
+  ).toBeVisible();
+  await expect(page.getByText("Not quite game of the year,")).toBeVisible();
+  await expect(page.getByRole("button", { name: "Sign In" })).toBeVisible();
 });
