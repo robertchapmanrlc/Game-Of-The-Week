ff15d4d999825595ab4b0b75b5c3c5d186cfac2d
Robert Chapman <chapmanrobert12@gmail.com>
2024-05-01 09:53:38 -0500

    The query actions will now check if a user is calling the methods

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -1,14 +1,27 @@
 import "server-only";
 import { db } from "./db";
-import { games } from "./db/schema";
 import { eq } from "drizzle-orm";
+import { games } from "./db/schema";
+import { auth } from "@clerk/nextjs/server";
 
 export async function getGames() {
+  const { userId } = auth();
+
+  if (!userId) {
+    throw Error('Unauthorized');
+  }
+
   const games = await db.query.games.findMany({ orderBy: (model, { desc }) => desc(model.id)});
   return games;
 }
 
 export async function registerVote(count: number, name: string) {
+  const { userId } = auth();
+
+  if (!userId) {
+    throw Error("Unauthorized");
+  }
+
   await db.update(games).set({ votes: count + 1 }).where(eq(games.name, name));
 }
 
