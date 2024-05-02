8799989d153b684ea6e6f61adb82e9991d9bacca
Robert Chapman <chapmanrobert12@gmail.com>
2024-05-02 12:25:04 -0500

    Users who have and haven't voted should be able to see the same UI when the voting period ends

--- src/app/components/game.tsx
+++ src/app/components/game.tsx
@@ -29,7 +29,7 @@ export default async function Game({ game, id }: { game: Game; id: number }) {
         alt={`Game ${id}`}
         priority
       />
-      {!voted && (
+      {(!voted && game.winner == undefined) && (
         <form
           className="w-full"
           action={async () => {
@@ -45,7 +45,7 @@ export default async function Game({ game, id }: { game: Game; id: number }) {
           </button>
         </form>
       )}
-      {voted && (
+      {(voted || game.winner != undefined) && (
         <div className="w-full py-1 md:py-2 text-lg md:text-xl lg:text-3xl text-text text-center font-bebasneue bg-accent rounded-md">
           {game.votes}
         </div>
