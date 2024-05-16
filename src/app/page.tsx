9ce9cc6a2a2e027711bd9139ef271eb713ab1077
Robert Chapman <chapmanrobert12@gmail.com>
2024-05-15 23:14:34 -0500

    Created grid layout template for the images on the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,15 +11,25 @@ export default async function Home() {
   }
 
   return (
-    <main className="w-full sm:py-20 md:py-10 lg:py-5 flex flex-col items-center justify-center gap-y-5 px-8">
+    <main className="w-full py-5 flex flex-col items-center justify-center gap-y-5 px-8">
       <h1 className="text-text font-bebasneue text-3xl md:text-5xl lg:text-7xl">
         Game of the Week
       </h1>
-      <div className="flex flex-col justify-center items-center gap-y-10">
+      <div className="w-full flex flex-col justify-center items-center gap-y-10">
         <h2 className="text-gray-400 font-bebasneue text-center text-sm md:text-xl lg:text-2xl">
           Not quite game of the year, but there is still value in being game of
           the week right?
         </h2>
+        <div className="w-full max-w-3xl h-[400px] md:h-[450px] lg:h-[500px] xl:h-[550px] grid grid-flow-dense grid-cols-12 grid-rows-12 gap-2">
+          <div className="p-4 col-span-4 row-span-7 bg-red-800"></div>
+          <div className="p-4 col-span-5 row-span-4 bg-blue-800"></div>
+          <div className="p-4 col-span-3 row-span-2 bg-yellow-500"></div>
+          <div className="p-4 col-span-3 row-span-2 bg-green-800"></div>
+          <div className="p-4 col-span-8 row-span-3 bg-purple-800"></div>
+          <div className="p-4 col-span-8 row-span-2 bg-orange-700"></div>
+          <div className="p-4 col-span-8 row-span-3 bg-pink-400"></div>
+          <div className="p-4 col-span-4 row-span-5 bg-white"></div>
+        </div>
         <SignedOut>
           <SignInButton
             forceRedirectUrl="/voting"
